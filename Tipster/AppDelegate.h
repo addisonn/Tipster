513b0d2d382d9b81a191fe15958fd20af33665ac
Xinyi Zhang <addisonz@fb.com>
2019-06-25 14:11:26 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by addisonz on 6/25/19.
+//  Copyright © 2019 addisonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
